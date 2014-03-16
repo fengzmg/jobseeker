0b6e256b810f3365a4ac1bd86ee89cf92a057913
mengfeng <mengfeng0904@gmail.com>
2014-03-16 21:38:27 +0800

    Improved the speed for search by using Multi-threaded search

--- jobseeker/multithreaded_jobseeker.py
+++ jobseeker/multithreaded_jobseeker.py
@@ -6,7 +6,7 @@ import logging
 import random
 import re
 from pyquery import PyQuery as pq
-
+import threading
 BASE_URL='http://www.recruit.net'
 URL='http://www.recruit.net/search.html'
 USER_AGENTS = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
@@ -27,6 +27,8 @@ Link: %s
 ===============================================================================
 '''
 
+thread_lock = threading.Lock()
+
 class JobItem:
     def __init__(self, title, href, company, location, description, source, post_time):
         self.title = title
@@ -38,62 +40,91 @@ class JobItem:
         self.post_time = post_time
 
 
-def extract_job_items(url, params, max_no):
-
-    job_items=[]
-    res = perform_http_request(url, params)
-    if res is None:
-        return []
+class JobExtractionThread(threading.Thread):
+    def __init__(self, threadID, name, url, params, job_items):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.url = url
+        self.params = params
+        self.job_items = job_items        
     
-    d = pq(res.text)
-    for index, job_item_pq_obj in enumerate(d('div').filter('.job')):
+    def extract_job_items(self, url, params):
+        extracted_job_items=[]
+        res = perform_http_request(url, params)
+        if res is None:
+            return []   
         
-        d_job_item = pq(job_item_pq_obj)
+        d = pq(res.text)
+        for index, job_item_pq_obj in enumerate(d('div').filter('.job')):
+            
+            d_job_item = pq(job_item_pq_obj)
 
-        item_link = d_job_item.find('h2').find('a')
-        item_company = d_job_item.find('span').filter('.company')
-        item_location = d_job_item.find('span').filter('.location').find('span')
-        item_description = d_job_item.find('div').filter('.description')
-        item_source = d_job_item.find('div').filter('.source')
-        item_post_time = d_job_item.find('div').filter('.time')
+            item_link = d_job_item.find('h2').find('a')
+            item_company = d_job_item.find('span').filter('.company')
+            item_location = d_job_item.find('span').filter('.location').find('span')
+            item_description = d_job_item.find('div').filter('.description')
+            item_source = d_job_item.find('div').filter('.source')
+            item_post_time = d_job_item.find('div').filter('.time')
 
-        title = item_link.attr.title
-        href = item_link.attr.href
-        company = item_company.text()
-        location = item_location.text()
-        description = item_description.text()
-        source = item_source.text()
+            title = item_link.attr.title
+            href = item_link.attr.href
+            company = item_company.text()
+            location = item_location.text()
+            description = item_description.text()
+            source = item_source.text()
+            
+            post_time_matches=re.compile('\d+').search(item_post_time.text())
+            post_time = post_time_matches.group() if post_time_matches else 'Unknown'
+
+            if title is not None: 
+                job_item = JobItem(title, href, company, location, description, source, post_time)
+                extracted_job_items.append(job_item) 
         
-        post_time_matches=re.compile('\d+').search(item_post_time.text())
-        post_time = post_time_matches.group() if post_time_matches else 'Unknown'
-        #post_time = item_post_time.text()
+        return extracted_job_items
 
-        #logging.debug(CONSOLE_OUTPUT_TEMPLATE % (index, source, post_time, title, 
-        #                                        company, location, '',''))
 
-        if title is not None:
-            job_item = JobItem(title, href, company, location, description, source, post_time)
-            job_items.append(job_item)
-    
-    #check for the link next
-    link_to_next_page= d('div').filter('.pagination').find('a').filter('.next')
-    
-    if len(job_items) >= max_no:
+    def run(self):
+        logging.info("starting " + self.name)
+        #thread_lock.acquire()
+        retrieved_items = self.extract_job_items(self.url, self.params) 
+        #thread_lock.release()
+        #print self.name, 'Number of jobs',len(retrieved_items) 
+        self.job_items += retrieved_items
+
+
+
+def extract_all_job_items(url, params):
+
+        job_items=[]
+        res = perform_http_request(url, params)
+        if res is None:
+            return []   
+        
+        total_pages = int(re.search(r'totalPages=(\d+)', res.text).group(1))
+        logging.info('total_pages:' + str(total_pages))
+
+        threads_pool = []
+        for i in range(1, total_pages + 1):
+           new_thread_params = params.copy()
+           new_thread_params['pageNo'] = i
+           #print 'params to start with', params
+           new_thread = JobExtractionThread(i, "Thread-" + str(i), url, new_thread_params, job_items)      
+           new_thread.start()
+           threads_pool.append(new_thread)
+        
+        #wait for all threads to complete
+        for t in threads_pool:
+            #t.start()
+            t.join()
+
         return job_items
-    if link_to_next_page and (link_to_next_page.attr.disabled is None):
-        href_to_next_page=link_to_next_page.attr.href
-        logging.info('next page href: \n%s' % href_to_next_page)
-        return job_items + extract_job_items(BASE_URL + '/' + href_to_next_page, None, max_no - len(job_items))
-    else:
-        return job_items
-
-    
 
 
 def perform_http_request(url, params=None):
     try:
         user_agent = random.choice(USER_AGENTS)
-        logging.info('making http request to { url: %s, user_agent: %s }' % (url, user_agent))
+        logging.info('making http request to { url: %s, user_agent: %s , params: %s}' % (url, user_agent, params))
         if params:
             res=requests.get(url, headers={'User-Agent': user_agent}, params=params)
         else:
@@ -106,7 +137,7 @@ def perform_http_request(url, params=None):
 
 def get_jobsearch_result(url, params, max_no):
         
-        job_items = extract_job_items(url, params, max_no)
+        job_items = extract_all_job_items(url, params)
         job_items.sort( key=lambda job_item: int(job_item.post_time) if job_item.post_time.isdigit() else 100 )
         for index, job_item in enumerate(job_items[:max_no]):
             print (CONSOLE_OUTPUT_TEMPLATE % (index+1, job_item.source, job_item.post_time, 
